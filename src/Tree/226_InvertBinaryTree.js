3136128c215acb359d129078c23da63419bb7d88
GroverW <cheesemaker@gmail.com>
2020-08-25 19:54:41 -0700

    added 226 Invert Binary Tree

---
+++ src/Tree/226_InvertBinaryTree.js
@@ -0,0 +1,37 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+Trivia:
+This problem was inspired by this original tweet by Max Howell:
+
+Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+*/
+
+var invertTree = function(root) {
+  if(!root) return null;
+
+  const temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
