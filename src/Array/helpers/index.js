cc9cc11ab42c591347f50e803ba564423bec1756
GroverW <cheesemaker@gmail.com>
2020-09-05 22:20:10 -0700

    added randomize helper for Array

--- src/Array/helpers/index.js
+++ src/Array/helpers/index.js
@@ -1,10 +1,18 @@
 
-const printGrid = (grid) => (
+export const printGrid = (grid) => (
   `[
     ${grid.map((row) => printRow(row)).join(',\n')}
   ]`
 )
 
-const printRow = (row) => `[${row.join(',\t')}]`;
+export const printRow = (row) => `[${row.join(',\t')}]`;
 
-export { printGrid, printRow };
+export const randomize = (arr) => {
+  const randomized = [...arr];
+  for(let i = randomized.length - 1; i > 0; i -= 1) {
+    const randIdx = Math.floor(Math.random() * (i + 1));
+    [randomized[i], randomized[randIdx]] = [randomized[randIdx], randomized[i]];
+  }
+
+  return randomized;
+}
