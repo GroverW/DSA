43c5a3c4fbd25d0fc72924d6c03a19d9a8f21bac
GroverW <cheesemaker@gmail.com>
2020-09-08 20:12:03 -0700

    added Tree serialization methods

--- src/Tree/helpers/index.js
+++ src/Tree/helpers/index.js
@@ -1,29 +1,71 @@
-export const TreeNode = function(val) {
+export const TreeNode = function (val) {
   this.val = val;
   this.left = null;
   this.right = null;
 }
 
 export const buildTree = (vals) => {
-  if(!vals.length) return null;
+  if (!vals.length) return null;
   const root = new TreeNode(vals.shift())
   const queue = [root];
 
-  while(queue.length) {
+  while (queue.length) {
     const curr = queue.shift();
     const left = vals.shift();
     const right = vals.shift();
 
-    if(left !== null && left !== undefined) {
+    if (left !== null && left !== undefined) {
       curr.left = new TreeNode(left);
       queue.push(curr.left);
     }
 
-    if(right !== null && right !== undefined) {
+    if (right !== null && right !== undefined) {
       curr.right = new TreeNode(right);
       queue.push(curr.right);
     }
   }
 
   return root;
+}
+
+export const serializeTreeInOrder = (root) => {
+  const queue = [root];
+  const serialized = [];
+
+  while (queue.length) {
+    const curr = queue.shift();
+    serialized.push(curr.val);
+    if (curr.left) queue.push(curr.left);
+    if (curr.right) queue.push(curr.right);
+  }
+
+  return serialized;
+}
+
+export const serializeTreePreOrder = (root) => {
+  const stack = [root];
+  const serialized = [];
+
+  while (stack.length) {
+    const curr = stack.pop();
+    serialized.push(curr.val);
+    if (curr.right) stack.push(curr.right);
+    if (curr.left) stack.push(curr.left);
+  }
+
+  return serialized;
+}
+
+export const serializeTreePostOrder = (root) => {
+  const stack = [root];
+  const serialized = [];
+
+  while (stack.length) {
+    const curr = stack.pop();
+    serialized.unshift(curr.val);
+    if (curr.left) stack.push(curr.left);
+    if (curr.right) stack.push(curr.right);
+  }
+
+  return serialized;
 }
