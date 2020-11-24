39439e669477db9f592bae6033c05c73eb1fd7ac
GroverW <cheesemaker@gmail.com>
2020-11-23 20:06:35 -0800

    removed logOut as it's not being used

--- src/logging/index.js
+++ src/logging/index.js
@@ -3,10 +3,6 @@ const outputList = document.getElementById('output-list');
 output.innerText = '';
 outputList.innerText = '';
 
-export const logOut = (data) => {
-  output.innerText = data;
-}
-
 export const logOutList = (data) => {
   const listItem = document.createElement('li')
   listItem.innerText = data;
