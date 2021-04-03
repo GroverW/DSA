a56fe7bbfd10b483579347cfddff8cf4b6b54956
GroverW <cheesemaker@gmail.com>
2021-04-03 14:59:08 -0700

    added 1334 Find The City With The Smallest Number Of Neighbors At A Threshold Distance

---
+++ src/Graph/1334_FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.js
@@ -0,0 +1,97 @@
+/*
+There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
+
+Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
+
+Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4
+Output: 3
+Explanation: The figure above describes the graph. 
+The neighboring cities at a distanceThreshold = 4 for each city are:
+City 0 -> [City 1, City 2] 
+City 1 -> [City 0, City 2, City 3] 
+City 2 -> [City 0, City 1, City 3] 
+City 3 -> [City 1, City 2] 
+Cities 0 and 3 have 2 neighboring cities at a distanceThreshold = 4, but we have to return city 3 since it has the greatest number.
+Example 2:
+
+
+Input: n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2
+Output: 0
+Explanation: The figure above describes the graph. 
+The neighboring cities at a distanceThreshold = 2 for each city are:
+City 0 -> [City 1] 
+City 1 -> [City 0, City 4] 
+City 2 -> [City 3, City 4] 
+City 3 -> [City 2, City 4]
+City 4 -> [City 1, City 2, City 3] 
+The city 0 has 1 neighboring city at a distanceThreshold = 2.
+ 
+
+Constraints:
+
+2 <= n <= 100
+1 <= edges.length <= n * (n - 1) / 2
+edges[i].length == 3
+0 <= fromi < toi < n
+1 <= weighti, distanceThreshold <= 10^4
+All pairs (fromi, toi) are distinct.
+*/
+
+var findTheCity = function(n, edges, distanceThreshold) {
+  const MAX = 10 ** 9;
+  const [adjList, adjMatrix] = buildAdjacencies(n, edges, distanceThreshold);
+  
+  let best = 0;
+  let minReachable = n;
+  
+  for(let i = 0; i < n; i += 1) {
+    const distances = new Array(n).fill(MAX);
+    distances[i] = 0;
+    let numReachable = 0;
+    const queue = [i];
+    while(queue.length) {
+      if (numReachable > minReachable) break;
+      const current = queue.shift();
+      const distance = distances[current];
+      
+      for (let neighbor of adjList[current]) {
+        const nextDistance = distance + adjMatrix[current][neighbor];
+        if (
+          nextDistance > distanceThreshold
+          || nextDistance >= distances[neighbor]
+        ) continue;
+        if (distances[neighbor] === MAX) numReachable += 1;
+        distances[neighbor] = nextDistance;
+        queue.push(neighbor);
+      }
+    }
+    
+    if (numReachable <= minReachable) {
+      best = i;
+      minReachable = numReachable;
+    }
+  }
+  
+  return best;
+};
+
+const buildAdjacencies = (n, edges, maxWeight) => {
+  const adjList = new Array(n).fill(0).map(() => []);
+  const adjMatrix = new Array(n).fill(0)
+    .map(() => new Array(n).fill(0))
+  for(let [from, to, weight] of edges) {
+    if (weight > maxWeight) continue;
+    adjList[from].push(to);
+    adjList[to].push(from);
+    adjMatrix[from][to] = weight;
+    adjMatrix[to][from] = weight;
+  }
+  return [adjList, adjMatrix];
+}
